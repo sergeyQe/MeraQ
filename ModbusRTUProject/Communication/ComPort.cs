5a128a07400ef19719223842f397398329aeb7a0
hghgrdghtdrtrthry <pollosatyi@mail.ru>
2026-02-10 13:55:42 +0300

    инициализация класса ComPort

---
+++ ModbusRTUProject/Communication/ComPort.cs
@@ -0,0 +1,42 @@
+﻿using ModbusRTUProject.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModbusRTUProject.Communication
+{
+    internal class ComPort : ICommunicator
+    {
+        public void Close()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DiscardIOBuffers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Open()
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Read(byte[] buffer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] Read(int byteCount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Write(byte[] buffer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
