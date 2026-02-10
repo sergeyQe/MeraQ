5fb36363a7e95459bd758fd88845cf385973a493
hghgrdghtdrtrthry <pollosatyi@mail.ru>
2026-02-10 14:01:57 +0300

    инициализация класса Modbus

---
+++ ModbusRTUProject/Communication/Modbus.cs
@@ -0,0 +1,21 @@
+﻿using ModbusRTUProject.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModbusRTUProject.Communication
+{
+    internal class Modbus
+    {
+        private readonly ICommunicator _communicator;
+
+        public Modbus(ICommunicator communicator)
+        {
+            _communicator = communicator;
+        }
+
+
+    }
+}
