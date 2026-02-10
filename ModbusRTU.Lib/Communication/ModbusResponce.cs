7fc2dcd2946d51d260ab2638a4c575db516240a4
hghgrdghtdrtrthry <pollosatyi@mail.ru>
2026-02-10 15:26:46 +0300

    реализация enum ExceptionCode

---
+++ ModbusRTU.Lib/Communication/ModbusResponce.cs
@@ -0,0 +1,14 @@
+﻿using ModbusRTU.Lib.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModbusRTUProject.Communication
+{
+    internal class ModbusResponce
+    {
+        public ExceptionCode Error {  get; set; }
+    }
+}
