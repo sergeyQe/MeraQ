5c4438bf13841a66b90c1acb98e7ca2708b75393
hghgrdghtdrtrthry <pollosatyi@mail.ru>
2026-02-12 20:51:59 +0300

    Реализация демонстрации проекта

--- ModbusRTU.Lib/Communication/Modbus.cs
+++ ModbusRTU.Lib/Communication/Modbus.cs
@@ -1,11 +1,5 @@
 ﻿using ModbusRTU.Lib.Enums;
 using ModbusRTUProject.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace ModbusRTUProject.Communication
 {
--- ModbusRTU.Lib/Communication/ModbusResponse.cs
+++ ModbusRTU.Lib/Communication/ModbusResponse.cs
@@ -1,16 +1,59 @@
 ﻿using ModbusRTU.Lib.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace ModbusRTUProject.Communication
 {
     public class ModbusResponse
     {
-        public ExceptionCode Error { get; set; }
+        public ExceptionCode Error { get; set; } // Код ошибки в соответствии со спецификацией протокола Modbus RTU
 
-        public byte[] Data { get; set; }
+        public byte[] Data { get; set; }  // Массив байтов ответа на запрос
+
+        public ModbusResponse()
+        {
+            Error = ExceptionCode.Success;
+            Data = Array.Empty<byte>();
+        }
+
+        public ModbusResponse(ExceptionCode error, byte[] data)
+        {
+            Error = error;
+            Data = data ?? Array.Empty<byte>();
+        }
+
+        // Является ли ответ успешным
+        public bool IsSuccess => Error == ExceptionCode.Success;
+
+        // Возвращает текстовое описание ошибки
+        public string GetErrorDescription()
+        {
+            return Error switch
+            {
+                ExceptionCode.Success => "Успешно",
+                ExceptionCode.IllegalFunction => "Недопустимый код функции",
+                ExceptionCode.IllegalDataAddress => "Недопустимый адрес данных",
+                ExceptionCode.IllegalDataValue => "Недопустимое значение данных",
+                ExceptionCode.SlaveDeviceFailure => "Ошибка устройства",
+                ExceptionCode.Acknowledge => "Операция выполняется",
+                ExceptionCode.SlaveDeviceBusy => "Устройство занято",
+                ExceptionCode.NegativeAcknowledge => "Отрицательное подтверждение",
+                ExceptionCode.MemoryParityError => "Ошибка чётности памяти",
+                ExceptionCode.GatewayPathUnavailable => "Маршрут недоступен",
+                ExceptionCode.GatewayTargetDeviceFailedToRespond => "Шлюз не отвечает",
+                _ => $"Неизвестная ошибка (0x{(byte)Error:X2})"
+            };
+        }
+
+        // Перегрузка для красивого вывода
+        public override string ToString()
+        {
+            if (IsSuccess)
+            {
+                string dataHex = Data.Length > 0 ? BitConverter.ToString(Data) : "пусто";
+                return $"ModbusResponse: Успех, Data[{Data.Length}] = {dataHex}";
+            }
+            return $"ModbusResponse: Ошибка = {Error} ({GetErrorDescription()})";
+        }
+
     }
 }
